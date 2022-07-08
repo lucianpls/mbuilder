8c302323f8cdfdaf38e03dd8df0eae90cc1346dc
Lucian Plesea <LPlesea@esri.com>
2022-07-08 13:17:08 -0700

    compute data histograms

---
+++ do_hist.py
@@ -0,0 +1,14 @@
+import sys
+from osgeo import gdal
+
+pszFilename = sys.argv[1]
+if pszFilename:
+    hDataset = gdal.Open( pszFilename, gdal.GA_ReadOnly )
+
+    for i in range(hDataset.RasterCount):
+        hBand = hDataset.GetRasterBand(i+1)
+        (lo, hi, avg, std) = hBand.ComputeStatistics(False)
+        print(f"Band {i} min={lo} max={hi} avg={avg} dev={std}")
+
+        hist = hBand.GetHistogram(min=-0.5, max=hi+0.5, buckets=int(hi+1), approx_ok = False)
+        print(hist)
